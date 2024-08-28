51478457ed5f2781efca5e945c3ce4e8e93e23b9
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2024-08-28 21:57:39 +0800

    Update register.php

--- app/Views/Home/register.php
+++ app/Views/Home/register.php
@@ -71,10 +71,12 @@
                   <div class="col-md-3 col-3">
                     <label for="middlename" class="form-label">Initial</label>
                     <div class="input-group has-validation">
-                      <input type="text" name="middlename" class="form-control " id="middlename" required>
-                      <div class="invalid-feedback">Please Enter Middle Initial!</div>
+                      <input type="text" name="middlename" class="form-control" id="middlename" pattern="[A-Z]"
+                        maxlength="1" required>
+                      <div class="invalid-feedback">Please enter a single uppercase letter for the middle initial!</div>
                     </div>
                   </div>
+
 
                   <div class="col-md-6 col-6">
                     <label for="yourUsername" class="form-label">Username</label>
