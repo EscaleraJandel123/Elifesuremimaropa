f22714a92cdd495db92b885f090aa9009da3b23f
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2024-08-28 22:35:49 +0800

    Update HomepageController.php

--- app/Controllers/HomepageController.php
+++ app/Controllers/HomepageController.php
@@ -133,9 +133,9 @@ class HomepageController extends BaseController
                 // $this->notifcont->newnotif($userId, $link, $message, $r);
                 $this->confirm->save($applicantData);
             }
-            $emailSubject = "Account Registration Confirmation";
-            $emailMessage = "Thank you for registering! Your account is currently registered. Please wait for confirmation from the admin before you can log in.";
-            $this->sendVerificationEmail($this->request->getVar('email'), $emailSubject, $emailMessage);
+            // $emailSubject = "Account Registration Confirmation";
+            // $emailMessage = "Thank you for registering! Your account is currently registered. Please wait for confirmation from the admin before you can log in.";
+            // $this->sendVerificationEmail($this->request->getVar('email'), $emailSubject, $emailMessage);
             return redirect()->to('/login')->with('success', 'Account Registered please be patient. An email has been sent to your registered email address.');
 
         } else {
