2eba1c560d7f09e576bd2604985539bdf86103b1
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2024-08-28 23:49:46 +0800

    Update head.php

--- app/Views/head.php
+++ app/Views/head.php
@@ -4,7 +4,7 @@
     <meta name="description" content="">
     <meta name="author" content="Tooplate">
 
-    <title>ALLIANZ PNB</title>
+    <title>ALLIANZ PNB Mimaropa</title>
 
     <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
     <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
