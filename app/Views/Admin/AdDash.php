024015f43a21daf82f56c31c359c055f4a45f1b6
JandelIsPogi <jandelpogi@gmail.com>
2024-09-19 19:04:36 +0800

    Update AdDash.php

--- app/Views/Admin/AdDash.php
+++ app/Views/Admin/AdDash.php
@@ -113,37 +113,34 @@
                                 </div>
                             </div>
 
-                            <!-- <div class="col-lg-12 col-12">
+                            <div class="col-lg-12 col-12">
                                 <div class="card">
                                     <div class="card-body">
                                         <h5 class="card-title">Table with hoverable rows</h5>
                                         <div class="table-responsive">
                                             <table class="table table-hover">
                                                 <thead>
-                                                    <tr>
-                                                        <th scope="col">#</th>
-                                                        <th scope="col">Name</th>
-                                                        <th scope="col">Position</th>
-                                                        <th scope="col">Age</th>
-                                                        <th scope="col">Start Date</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th scope="row">1</th>
-                                                        <td>Brandon Jacob</td>
-                                                        <td>Designer</td>
-                                                        <td>28</td>
-                                                        <td>2016-05-25</td>
-                                                    </tr>
-
-                                                </tbody>
+                                                    <?php foreach ($alluser as $users): ?>
+                                                        <tr>
+                                                            <th scope="col">#</th>
+                                                            <th scope="col">User Name</th>
+                                                            <th scope="col">Position</th>
+                                                            <th scope="col">Age</th>
+                                                            <th scope="col">Start Date</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <th scope="row">1</th>
+                                                            <td><?= $users['username'] ?></td>
+                                                        </tr>
+                                                    </tbody>
+                                                <?php endforeach; ?>
                                             </table>
-
                                         </div>
                                     </div>
                                 </div>
-                            </div> -->
+                            </div>
                         </div>
                     </div>
                     <!-- end of left side -->
