d4af801f324d93a6fc80c756927e63431126cc96
JandelIsPogi <jandelpogi@gmail.com>
2024-09-19 21:39:21 +0800

    Update reports.php

--- app/Views/Admin/reports.php
+++ app/Views/Admin/reports.php
@@ -108,7 +108,7 @@
                                 <div class="card">
                                     <!-- Table with hoverable rows -->
                                     <div class="table-responsive mx-3">
-                                        <h5 class="card-title mt-2">Agents</h5>
+                                        <h5 class="card-title mt-4">Agents</h5>
                                         <table class="table table-hover">
                                             <thead>
                                                 <tr>
