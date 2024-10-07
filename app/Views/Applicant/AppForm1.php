3f12151f3ac7a306fb4d69c378aa216b9ca90b69
Jandel <jandel@gmail.com>
2024-10-07 21:26:26 +0800

    Update AppForm1.php

--- app/Views/Applicant/AppForm1.php
+++ app/Views/Applicant/AppForm1.php
@@ -89,7 +89,7 @@
                                                 <div class="form-group">
                                                     <label>Source:</label><br>
                                                     <input type="checkbox" id="referral" name="referral" value="yes"
-                                                        <?= isset($lifechangerform['referral']) && $lifechangerform['referral'] === 'yes' ? 'checked' : 'checked' ?>>
+                                                        <?= isset($lifechangerform['referral']) && $lifechangerform['referral'] === 'yes' ? 'checked' : 'checked' ?> readonly>
                                                     <label for="referral">Referral</label>
                                                     <label for="referralBy">by whom:</label>
 
