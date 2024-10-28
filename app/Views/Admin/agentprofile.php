260b28605c6c5eb64e26e9621c8cf5f090e07418
Jandel <jandel@gmail.com>
2024-10-29 01:54:45 +0800

    Update agentprofile.php

--- app/Views/Admin/agentprofile.php
+++ app/Views/Admin/agentprofile.php
@@ -352,9 +352,9 @@
                                                                         <i class="fas <?= $iconClass ?> fa-3x"></i>
                                                                     </a>
                                                                 </p>
-                                                                <!-- <a href="<?= $filePath ?>" target="_blank" class="btn btn-link">
+                                                                <a href="<?= $filePath ?>" target="_blank" class="btn btn-link">
                                                                     <span style="font-size: 9px;"><?= $files["file$i"] ?></span>
-                                                                </a> -->
+                                                                </a>
                                                             </div>
                                                         </div>
                                                     </div>
