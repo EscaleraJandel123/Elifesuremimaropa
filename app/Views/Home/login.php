025e800f0e98c0dab53b4436a8aed479f949474d
Jandel <jandel@gmail.com>
2024-10-31 11:42:59 +0800

    Update login.php

--- app/Views/Home/login.php
+++ app/Views/Home/login.php
@@ -16,7 +16,7 @@
                             <div class="d-flex justify-content-center py-4">
                                 <a href="/" class="logo d-flex align-items-center w-auto">
                                     <img src="<?= base_url() ?>req/allianzlogo1.png" alt="">
-                                    <span class="d-none d-lg-block">ALLIANZ PNB</span>
+                                    <span class="d-none d-lg-block">ALLIANZ LIFER</span>
                                 </a>
                             </div><!-- End Logo -->
                             <div class="card mb-3 small">
