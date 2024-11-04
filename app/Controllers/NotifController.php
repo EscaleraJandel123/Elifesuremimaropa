8c12581eefe9fd3b4dfa7ed376490dd76b6fd93b
Jandel <jandel@gmail.com>
2024-11-04 23:27:02 +0800

    s

--- app/Controllers/NotifController.php
+++ app/Controllers/NotifController.php
@@ -99,7 +99,7 @@ class NotifController extends BaseController
         //     'response' => $decodedResponse
         // ]);
 
-        var_dump(env("SEMAPHORE_API_KEY"));
+        var_dump(env("CI_ENVIRONMENT"));
     }
 
 }
