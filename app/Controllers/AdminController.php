1db8c9826cac61c030d23a67bd56e173ba0495fc
Jandel <jandel@gmail.com>
2024-11-04 09:13:59 +0800

    Update AdminController.php

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -576,7 +576,8 @@ class AdminController extends BaseController
     public function schededit($id)
     {
         $data = array_merge($this->getData(), $this->getDataAd(), $this->notifcont->notification());
-        $data['schedules'] = $this->scheduleModel->where('id' , $id)->find();
+        $data['schedules'] = $this->scheduleModel->findAll();
+        $data['sched'] = $this->scheduleModel->where('id' , $id)->find();
         return view('Admin/Schedule', $data); // Replace 'your_view_path' with the actual view file where the form is located
     }
 
