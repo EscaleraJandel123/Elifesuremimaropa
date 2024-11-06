c84696cee2beac9a3c690d4296a869ce71207f76
Jandel <jandel@gmail.com>
2024-11-07 07:58:42 +0800

    Update NotifController.php

--- app/Controllers/NotifController.php
+++ app/Controllers/NotifController.php
@@ -57,7 +57,7 @@ class NotifController extends BaseController
 
     public function sendNotification()
     {
-        $to = '09955435819';
+        $to = '09366581432';
         $message = 'Welcome to Elifesure! Thank you for choosing us as your agency partner. We are here to serve you with excellence. For assistance. Mabuhay!
 ';
 
