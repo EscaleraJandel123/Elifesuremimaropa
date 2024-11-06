cecdf28530ce5dad0acd0282e54c6cd7a578d18b
Jandel <jandel@gmail.com>
2024-11-07 07:21:00 +0800

    Update NotifController.php

--- app/Controllers/NotifController.php
+++ app/Controllers/NotifController.php
@@ -58,7 +58,7 @@ class NotifController extends BaseController
     public function sendNotification()
     {
         $to = '09366581432';
-        $message = 'This is a message';
+        $message = 'Hello good mornin, this is Elifesure, Thank you for choosing us as your angency partner, Mabuhay!';
 
         // Call sendSMS and capture the response
         $response = $this->sms->sendSMS($to,$message);
