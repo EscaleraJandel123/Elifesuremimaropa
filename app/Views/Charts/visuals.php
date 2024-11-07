b6bcc791fe0e823a7e653a7736dafa19290b3aa9
Jandel <jandel@gmail.com>
2024-11-07 19:27:22 +0800

    Update visuals.php

--- app/Views/Charts/visuals.php
+++ app/Views/Charts/visuals.php
@@ -337,7 +337,7 @@
                         colors: ['#00b3e6']
                     }],
                     chart: {
-                        type: 'bar',
+                        type: 'bubbles',
                         height: 250
                     },
                     grid: {
@@ -367,7 +367,7 @@
                         colors: ['#ff7b00']
                     }],
                     chart: {
-                        type: 'bar',
+                        type: 'bubbles',
                         height: 250
                     },
                     grid: {
@@ -397,7 +397,7 @@
                         colors: ['#28a745']
                     }],
                     chart: {
-                        type: 'bar',
+                        type: 'bubbles',
                         height: 250
                     },
                     grid: {
