5e3e249bf695400c5f89b153ad1e41f356cfb514
Jandel <jandel@gmail.com>
2024-11-07 18:12:50 +0800

    Update visuals.php

--- app/Views/Charts/visuals.php
+++ app/Views/Charts/visuals.php
@@ -63,6 +63,70 @@
     }
 </script>
 
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        fetch('/getApplicantsCount')
+            .then(response => response.json())
+            .then(data => {
+                if (data.length === 0) {
+                    document.querySelector("#ApplicantChart").innerHTML = '<div style="text-align: center; padding: 20px;">No Data Available</div>';
+                } else {
+                    const monthsYears = data.map(item => `${getMonthName(item.month)} ${item.year}`);
+                    const applicantCounts = data.map(item => item.applicant_count);
+
+                    new ApexCharts(document.querySelector("#ApplicantChart"), {
+                        series: [{
+                            name: 'Number of Applicants',
+                            data: applicantCounts
+                        }],
+                        chart: {
+                            type: 'pie',
+                            height: 250
+                        },
+                        plotOptions: {
+                            bar: {
+                                borderRadius: 4,
+                                horizontal: false,
+                            }
+                        },
+                        dataLabels: {
+                            enabled: false
+                        },
+                        xaxis: {
+                            categories: monthsYears,
+                            labels: {
+                                rotate: -45,
+                                offsetY: 5,
+                                style: {
+                                    fontSize: '12px'
+                                }
+                            }
+                        },
+                        grid: {
+                            show: false // Hide grid lines
+                        },
+                        yaxis: {
+                            title: {
+                                text: 'Number of Applicants'
+                            },
+                            labels: {
+                                formatter: function (value) {
+                                    return Math.floor(value); // Remove decimals
+                                }
+                            }
+                        },
+
+                    }).render();
+                }
+            });
+    });
+
+    function getMonthName(monthNumber) {
+        const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+        return months[monthNumber - 1];
+    }
+</script>
+
 
 
 <script>
@@ -159,69 +223,7 @@
     });
 </script>
 
-<script>
-    document.addEventListener("DOMContentLoaded", () => {
-        fetch('/getApplicantsCount')
-            .then(response => response.json())
-            .then(data => {
-                if (data.length === 0) {
-                    document.querySelector("#ApplicantChart").innerHTML = '<div style="text-align: center; padding: 20px;">No Data Available</div>';
-                } else {
-                    const monthsYears = data.map(item => `${getMonthName(item.month)} ${item.year}`);
-                    const applicantCounts = data.map(item => item.applicant_count);
 
-                    new ApexCharts(document.querySelector("#ApplicantChart"), {
-                        series: [{
-                            name: 'Number of Applicants',
-                            data: applicantCounts
-                        }],
-                        chart: {
-                            type: 'pie',
-                            height: 250
-                        },
-                        plotOptions: {
-                            bar: {
-                                borderRadius: 4,
-                                horizontal: false,
-                            }
-                        },
-                        dataLabels: {
-                            enabled: false
-                        },
-                        xaxis: {
-                            categories: monthsYears,
-                            labels: {
-                                rotate: -45,
-                                offsetY: 5,
-                                style: {
-                                    fontSize: '12px'
-                                }
-                            }
-                        },
-                        grid: {
-                            show: false // Hide grid lines
-                        },
-                        yaxis: {
-                            title: {
-                                text: 'Number of Applicants'
-                            },
-                            labels: {
-                                formatter: function (value) {
-                                    return Math.floor(value); // Remove decimals
-                                }
-                            }
-                        },
-
-                    }).render();
-                }
-            });
-    });
-
-    function getMonthName(monthNumber) {
-        const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-        return months[monthNumber - 1];
-    }
-</script>
 
 
 <script>
