6453480a874044c7494c400970d03bc5aec33225
Jandel <jandel@gmail.com>
2024-11-07 08:13:47 +0800

    Update SemaphoreService.php

--- app/Libraries/SemaphoreService.php
+++ app/Libraries/SemaphoreService.php
@@ -31,7 +31,7 @@ class SemaphoreService
             // 'sendername' => $this->semaphoreSenderName,
         ];
 
-        curl_setopt($ch, CURLOPT_URL, 'https://api.semaphore.co/api/v3/messages');
+        curl_setopt($ch, CURLOPT_URL, 'https://api.semaphore.co/api/v4/messages');
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
