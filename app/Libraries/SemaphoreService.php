961e804e0a5cc6625ceb2ef7232d791f3a07de11
Jandel <jandel@gmail.com>
2024-11-07 09:22:46 +0800

    Update SemaphoreService.php

--- app/Libraries/SemaphoreService.php
+++ app/Libraries/SemaphoreService.php
@@ -10,7 +10,7 @@ class SemaphoreService
     {
         $this->semaphoreApiKey = 'dfdb3f38323f2e2f0fca0d6ae9624fdb';
         // Optional: Set your Semaphore sender name here if it's approved and required.
-        // $this->semaphoreSenderName = 'ALLIANZ_PNB';
+        // $this->semaphoreSenderName = 'ALLIANZ_PNP';
     }
 
     public function sendSMS($to, $message)
