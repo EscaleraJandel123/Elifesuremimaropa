24e8ec31172cd1f264d69879a4354d0cd2ecbc20
Jandel <jandel@gmail.com>
2024-11-10 21:34:21 +0800

    Update register.php

--- app/Views/Home/register.php
+++ app/Views/Home/register.php
@@ -23,7 +23,7 @@
             <div class="d-flex justify-content-center py-4">
               <a href="/" class="logo d-flex align-items-center w-auto">
                 <img src="<?= base_url() ?>req/allianzlogo1.png" alt="">
-                <span class="d-none d-lg-block">ALLIANZ</span>
+                <span class="d-none d-lg-block">ALLIANZ PNB</span>
               </a>
             </div><!-- End Logo -->
 
