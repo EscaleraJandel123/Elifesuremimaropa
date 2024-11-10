ae7ea642b47eea5da66c46457642b2f71fce89b2
Jandel <jandel@gmail.com>
2024-11-10 15:44:30 +0800

    Update HomepageController.php

--- app/Controllers/HomepageController.php
+++ app/Controllers/HomepageController.php
@@ -137,7 +137,7 @@ class HomepageController extends BaseController
                 $to = $this->request->getVar('number'); 
                 $from = '447491163443';
                 $text = 'Thank you for registering! Your account is currently registered. Please wait for confirmation from the admin before you can log in.';
-                $response = $this->smsLibrary->sendSms($to, $from, $text);
+                // $response = $this->smsLibrary->sendSms($to, $from, $text);
                 $this->confirm->save($applicantData);
             }
 
