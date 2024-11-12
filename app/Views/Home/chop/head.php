3e85be753529da3f3114f199a68b331138ac9130
Jandel <jandel@gmail.com>
2024-11-12 14:08:23 +0800

    analytics

--- app/Views/Home/chop/head.php
+++ app/Views/Home/chop/head.php
@@ -1,4 +1,13 @@
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-0PQHSQFXS9"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-0PQHSQFXS9');
+  </script>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
@@ -12,7 +21,9 @@
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="<?= base_url() ?>req/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
--- app/Views/Home/chop1/head.php
+++ app/Views/Home/chop1/head.php
@@ -1,29 +1,38 @@
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-0PQHSQFXS9"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
 
-    <link href="<?= base_url() ?>req/allianzlogo1.png" rel="icon">
-    <link href="<?= base_url() ?>req/allianzlogo1.png" rel="apple-touch-icon">
+    gtag('config', 'G-0PQHSQFXS9');
+  </script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/animate.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/boxicons.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/meanmenu.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/fancybox.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/odometer.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/scrollCue.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/navbar.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/footer.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/style.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/dark.css">
-    <link rel="stylesheet" href="<?=base_url()?>client/assets/css/responsive.css">
-    <title>ALLIANZ PNB MIMAROPA</title>
-    <link rel="icon" type="image/png" href="<?= base_url(); ?>req/allianzlogo4.png">
-    <!-- <style>
+  <link href="<?= base_url() ?>req/allianzlogo1.png" rel="icon">
+  <link href="<?= base_url() ?>req/allianzlogo1.png" rel="apple-touch-icon">
+
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/animate.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/boxicons.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/magnific-popup.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/meanmenu.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/fancybox.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/odometer.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/scrollCue.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/navbar.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/footer.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/style.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/dark.css">
+  <link rel="stylesheet" href="<?= base_url() ?>client/assets/css/responsive.css">
+  <title>ALLIANZ PNB MIMAROPA</title>
+  <link rel="icon" type="image/png" href="<?= base_url(); ?>req/allianzlogo4.png">
+  <!-- <style>
     body {
       background-image: url('client/assets/images/allbg.png');
       background-size: 1000px;
@@ -31,4 +40,3 @@
     }
   </style> -->
 </head>
-
