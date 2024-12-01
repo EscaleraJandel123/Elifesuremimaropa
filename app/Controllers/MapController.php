d1f3402f5356234c8249bdcac0133c735a9cf38b
Jandel <jandel@gmail.com>
2024-12-01 22:55:49 +0800

    Update MapController.php

--- app/Controllers/MapController.php
+++ app/Controllers/MapController.php
@@ -166,27 +166,27 @@ class MapController extends BaseController
             ];
         }
 
+        // Count applicants in each city
         foreach ($applicants as $applicant) {
             if (isset($applicantCounts[$applicant['city']])) {
                 $applicantCounts[$applicant['city']]['count']++;
-                $applicantCounts[$applicant['city']]['role'] = 'Applicant';
-            }
-        }
-        
-        foreach ($clients as $client) {
-            if (isset($clientCounts[$client['city']])) {
-                $clientCounts[$client['city']]['count']++;
-                $clientCounts[$client['city']]['role'] = 'Client';
-            }
-        }
-        
-        foreach ($agents as $agent) {
-            if (isset($agentCounts[$agent['city']])) {
-                $agentCounts[$agent['city']]['count']++;
-                $agentCounts[$agent['city']]['role'] = 'Agent';
+                $applicant['role']="Applicant";
             }
         }
 
+        // Count clients in each city
+        foreach ($clients as $client) {
+            if (isset($clientCounts[$client['city']])) {
+                $clientCounts[$client['city']]['count']++;
+            }
+        }
+
+        // Count agents in each city
+        foreach ($agents as $agent) {
+            if (isset($agentCounts[$agent['city']])) {
+                $agentCounts[$agent['city']]['count']++;
+            }
+        }
         $data = $this->notifcont->notification();
         $data = array_merge($this->notifcont->notification(), $this->getDataAd());
         $data['applicantCounts'] = $applicantCounts;
